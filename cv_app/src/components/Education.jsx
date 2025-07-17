7defa062b1e89c4cc12351cda06a49cb2998f876
Anand <anand.work2005@gmail.com>
2025-07-17 21:36:07 +0530

    feat: Add the education section

--- cv_app/src/components/Education.jsx
+++ cv_app/src/components/Education.jsx
@@ -1,3 +1,4 @@
+import _default from "eslint-plugin-react-refresh";
 import { useState } from "react";
 
 function Education() {
@@ -6,16 +7,52 @@ function Education() {
   const [major, setMajor] = useState("");
   const [location, setLocation] = useState("");
 
+  function handleYear(e) {
+    setYear(e.target.value);
+  }
+
+  function handleUniversityName(e) {
+    setUniversityName(e.target.value);
+  }
+
+  function handleDegree(e) {
+    setMajor(e.target.value);
+  }
+
+  function handleLocation(e) {
+    setLocation(e.target.value);
+  }
+
   return (
     <>
       <div className="education">
         <div className="year">
-
           <label>
-            First Name: <input value={firstName} onChange={handleFirstName} placeholder="first name" />
+            Year: <input value={year} onChange={handleYear} placeholder="2xxx-xx (format)" />
           </label>
         </div>
+
+        <div className="university">
+          <label>
+            Unversity/College: <input value={universityName} onChange={handleUniversityName} placeholder="university name" />
+          </label>
+        </div>
+
+        <div className="degree">
+          <label>
+            Degree: <input value={major} onChange={handleDegree} placeholder="degree/major" />
+          </label>
+        </div>
+
+        <div className="location">
+          <label>
+            Location: <input value={location} onChange={handleLocation} placeholder="location" />
+          </label>
+        </div>
+
       </div>
     </>
   );
 }
+
+export default Education;
--- cv_app/src/main.jsx
+++ cv_app/src/main.jsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import Name from './components/Name.jsx'
+import Education from './components/Education.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
     <Name />
+    <Education />
   </StrictMode>,
 )
