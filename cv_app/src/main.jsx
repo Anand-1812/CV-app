254f1876d45b3cab1f7fc3afa4d1cf314bbffb2d
Anand <anand.work2005@gmail.com>
2025-07-18 12:15:17 +0530

    put the components to the App.jsx

--- cv_app/src/main.jsx
+++ cv_app/src/main.jsx
@@ -2,15 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Name from './components/Name.jsx'
-import Education from './components/Education.jsx'
-import Experience from './components/Experience.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-    <Name />
-    <Education />
-    <Experience />
   </StrictMode>,
 )
