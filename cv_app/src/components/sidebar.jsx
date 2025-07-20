754c687a2eaf70dd064f06b0ae18c4bc4e0ee663
Anand <anand.work2005@gmail.com>
2025-07-20 19:11:11 +0530

    use the state from App.jsx:

--- cv_app/src/components/sidebar.jsx
+++ cv_app/src/components/sidebar.jsx
@@ -4,40 +4,42 @@ import Name from "./Name.jsx";
 import Education from "./Education.jsx";
 import Experience from "./Experience.jsx";
 
-function Sidebar() {
+function Sidebar({
+  personalDetails,
+  setPersonalDetails,
+  educationDetails,
+  setEducationDetails,
+  experienceDetails,
+  setExperienceDetails,
+}) {
   const [activeSession, setActiveSession] = useState("personal");
-
-  const [personalDetails, setPersonalDetails] = useState({
-    firstName: "",
-    lastName: "",
-    email: "",
-    phone: "",
-  });
-
-  const [educationDetails, setEducationDetails] = useState({
-    year: "",
-    university: "",
-    degree: "",
-    location: "",
-  });
-
-  const [experienceDetails, setExperienceDetails] = useState({
-    year: "",
-    company: "",
-    title: "",
-    location: "",
-  });
 
   return (
     <section className="aside">
       <h1>CV Generator</h1>
       <Buttons setActiveSession={setActiveSession} />
-      {activeSession === "personal" && <Name personalDetails={personalDetails} setPersonalDetails={setPersonalDetails} />}
-      {activeSession === "education" && <Education personalDetails={educationDetails} setEducationDetails={setEducationDetails} />}
-      {activeSession === "experience" && <Experience experienceDetails={experienceDetails} setExperienceDetails={setExperienceDetails} />}
+      {activeSession === "personal" && (
+        <Name
+          personalDetails={personalDetails}
+          setPersonalDetails={setPersonalDetails}
+        />
+      )}
+      {activeSession === "education" && (
+        <Education
+          educationDetails={educationDetails}
+          setEducationDetails={setEducationDetails}
+        />
+      )}
+      {activeSession === "experience" && (
+        <Experience
+          experienceDetails={experienceDetails}
+          setExperienceDetails={setExperienceDetails}
+        />
+      )}
       {activeSession === "skills" && <p>Skills section will be added later</p>}
     </section>
   );
 }
 
 export default Sidebar;
+
