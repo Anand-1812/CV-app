4532d33adab555f8302c44bcdcebaf1d520dc0f3
Anand <anand.work2005@gmail.com>
2025-07-20 18:50:16 +0530

    lifting the state up

--- cv_app/src/components/Name.jsx
+++ cv_app/src/components/Name.jsx
@@ -1,10 +1,11 @@
-import { useState } from "react";
+function Name({ personalDetails, setPersonalDetails }) {
 
-function Name({ isActive }) {
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [email, setEmail] = useState("");
-  const [phone, setPhone] = useState("");
+  function handleChange(e) {
+    setPersonalDetails({
+      ...personalDetails,
+      [e.target.name]: e.target.value,
+    });
+  }
 
   return (
     <div className="inputSection">
@@ -12,19 +13,19 @@ function Name({ isActive }) {
         <h2>Personal Details</h2>
         <label>
           First Name:
-          <input value={firstName} onChange={(e) => setFirstName(e.target.value)} placeholder="first name" />
+          <input name="firstName" value={personalDetails.firstName} onChange={handleChange} placeholder="first name" />
         </label>
         <label>
           Last Name:
-          <input value={lastName} onChange={(e) => setLastName(e.target.value)} placeholder="last name" />
+          <input name="lastName" value={personalDetails.lastName} onChange={handleChange} placeholder="last name" />
         </label>
         <label>
           Email:
-          <input value={email} onChange={(e) => setEmail(e.target.value)} placeholder="email" />
+          <input name="email" value={personalDetails.email} onChange={handleChange} placeholder="email" />
         </label>
         <label>
           Phone No:
-          <input value={phone} onChange={(e) => setPhone(e.target.value)} placeholder="phone number" />
+          <input name="phone" value={personalDetails.phone} onChange={handleChange} placeholder="phone number" />
         </label>
       </div>
     </div>
