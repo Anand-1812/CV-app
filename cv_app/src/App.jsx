2669ef1e6f2edda1d25cc515c5cd295df46612b9
Anand <anand.work2005@gmail.com>
2025-07-20 19:03:49 +0530

    sidebar state to App.jsx

--- cv_app/src/App.jsx
+++ cv_app/src/App.jsx
@@ -3,6 +3,31 @@ import Sidebar from './components/sidebar.jsx'
 import CvSection from './components/CvSection.jsx'
 
 function App() {
+
+  const [personalDetails, setPersonalDetails] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    phone: "",
+  });
+
+  const [educationDetails, setEducationDetails] = useState({
+    year: "",
+    university: "",
+    degree: "",
+    location: "",
+  });
+
+  const [experienceDetails, setExperienceDetails] = useState({
+    year: "",
+    company: "",
+    title: "",
+    location: "",
+  });
+
+
+
+
   return (
     <>
       <Sidebar />
