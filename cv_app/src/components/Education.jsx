9bfdf71d1026ec562898cda310080a17886d8fbf
Anand <anand.work2005@gmail.com>
2025-07-20 19:00:02 +0530

    using state from the sidebar.jsx

--- cv_app/src/components/Education.jsx
+++ cv_app/src/components/Education.jsx
@@ -1,10 +1,11 @@
-import { useState } from "react";
+function Education({ educationDetails, setEducationDetails }) {
 
-function Education({ isActive }) {
-  const [year, setYear] = useState("");
-  const [universityName, setUniversityName] = useState("");
-  const [major, setMajor] = useState("");
-  const [location, setLocation] = useState("");
+  function handleChange(e) {
+    setEducationDetails({
+      ...educationDetails,
+      [e.target.name]: e.target.value,
+    })
+  }
 
   return (
     <div className="inputSection">
@@ -12,19 +13,19 @@ function Education({ isActive }) {
         <h2>Education Details</h2>
         <label>
           Year:
-          <input value={year} onChange={(e) => setYear(e.target.value)} placeholder="2xxx-xx (format)" />
+          <input name="year" value={educationDetails.year} onChange={handleChange} placeholder="2xxx-xx (format)" />
         </label>
         <label>
           University/College:
-          <input value={universityName} onChange={(e) => setUniversityName(e.target.value)} placeholder="university name" />
+          <input name="university" value={educationDetails.universit} onChange={handleChange} placeholder="university name" />
         </label>
         <label>
           Degree:
-          <input value={major} onChange={(e) => setMajor(e.target.value)} placeholder="degree/major" />
+          <input name="degree" value={educationDetails.degree} onChange={handleChange} placeholder="degree/major" />
         </label>
         <label>
           Location:
-          <input value={location} onChange={(e) => setLocation(e.target.value)} placeholder="location" />
+          <input name="location" value={educationDetails.location} onChange={handleChange} placeholder="location" />
         </label>
       </div>
     </div>
