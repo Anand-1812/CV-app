79e71ebfe1c4832db08b31a059af6497d2056883
Anand <anand.work2005@gmail.com>
2025-07-20 18:49:52 +0530

    lifting the state up

--- cv_app/src/components/sidebar.jsx
+++ cv_app/src/components/sidebar.jsx
@@ -7,13 +7,34 @@ import Experience from "./Experience.jsx";
 function Sidebar() {
   const [activeSession, setActiveSession] = useState("personal");
 
+  const [personalDetails, setPersonalDetails] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    phone: "",
+  });
+
+  const [educationDetails, setEducationDetails] = useState({
+    year: "",
+    university: "",
+    degree: "",
+    location: "",
+  });
+
+  const [experienceDetails, setExperienceDetails] = useState({
+    year: "",
+    company: "",
+    title: "",
+    location: "",
+  });
+
   return (
     <section className="aside">
       <h1>CV Generator</h1>
       <Buttons setActiveSession={setActiveSession} />
-      {activeSession === "personal" && <Name />}
-      {activeSession === "education" && <Education />}
-      {activeSession === "experience" && <Experience />}
+      {activeSession === "personal" && <Name personalDetails={personalDetails} setPersonalDetails={setPersonalDetails} />}
+      {activeSession === "education" && <Education personalDetails={educationDetails} setEducationDetails={setEducationDetails} />}
+      {activeSession === "experience" && <Experience experienceDetails={experienceDetails} setExperienceDetails={setExperienceDetails} />}
       {activeSession === "skills" && <p>Skills section will be added later</p>}
     </section>
   );
