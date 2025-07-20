13b6c1b623000c6c7e5c130e2aefec0967b8ba6f
Anand <anand.work2005@gmail.com>
2025-07-20 20:20:37 +0530

    add more details

--- cv_app/src/components/CvSection.jsx
+++ cv_app/src/components/CvSection.jsx
@@ -1,9 +1,15 @@
-export default function CvSection() {
+export default function CvSection({ personalDetails, educationDetails, experienceDetails }) {
   return (
     <>
       <section className="cv-section">
         <div className="cv">
-
+          <div className="top">
+            <h1>{personalDetails.firstName + ' ' + personalDetails.lastName}</h1><br />
+            <div className="userDetail">
+              <p>{personalDetails.email}</p>
+              <p>{personalDetails.phone}</p>
+            </div>
+          </div>
         </div>
       </section>
     </>
